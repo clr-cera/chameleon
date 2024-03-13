cd879cf9522b251ae9f3af48a6f7c0a905597dda
clr-cera <93736542+clr-cera@users.noreply.github.com>
2024-03-13 19:19:18 -0300

    fix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        metadata = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          name = "Chameleon";
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "arm-unknown-linux-gnueabihf" ];
+            })
+            rust-analyzer
+            pkg-config
+            gcc
+            just
+          ];
+          
+          shellHook = ''
+            fish
+          '';
+        };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = metadata.name;
+          version = metadata.version;
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+
+          nativeBuildInputs = [ pkg-config ];
+
+          buildInputs = [
+            openssl            
+            bzip2
+          ];
+          
+        };
+      }
+    );
+}
