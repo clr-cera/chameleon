2430dc3ca8e99a3916d1a384cab235a80d470882
clr <clrcera05@gmail.com>
2024-03-24 20:38:10 -0300

    remove sleep

--- src/data/themes/fixup/mod.rs
+++ src/data/themes/fixup/mod.rs
@@ -1,13 +1,9 @@
-use std::thread::sleep;
-use std::time::Duration;
-
 use super::Theme;
 use super::super::binaries::BinManager;
 impl Theme {
     pub fn fixup(&self) {
         for command in &self.data.fixup_inline {
             BinManager::execute(&command);
-            sleep(Duration::from_millis(20));
         }
 
         self.set_wallpaper();
